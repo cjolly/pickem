a8e44b2771e96f28f5e265b55bd6bf1972dae350
Chad Jolly <chad.a.jolly@gmail.com>
2012-12-08 14:36:23 -0700

    bin script to print picks

--- lib/pickem.rb
+++ lib/pickem.rb
@@ -1,13 +1,19 @@
 require 'rubygems'
 require 'bundler/setup'
 
-require 'active_record'
-require 'pickem/version'
-require 'pickem/week'
-require 'pickem/game'
-require 'pickem/team'
-require 'pickem/scraper'
+require 'pickem/scrapers/cbs_sports'
+require 'terminal-table'
 
 module Pickem
+  def self.print_picks
+    scraper = Scrapers::CBSSports.new
+    table = Terminal::Table.new({
+      title: scraper.source,
+      headings: ['Away', 'Home', 'Current Line'],
+      rows: scraper.sorted_games
+    })
 
+    table.align_column(2, :right)
+    puts table
+  end
 end
